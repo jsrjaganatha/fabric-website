8df90bf263f4cb0b89098770e2c98c2d7b09a055
Yaxue Guo <yaxue.guo23@gmail.com>
2020-12-21 14:16:27 -0500

    #14: added annotations for edge nodes

--- src/components/modules/map.js
+++ src/components/modules/map.js
@@ -9,6 +9,7 @@ import {
   Geography,
   Marker,
   Line,
+  Annotation,
 } from "react-simple-maps";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
@@ -60,7 +61,7 @@ const coordinates = {
   "Seattle": [-122.3321, 47.6062],
   "San Diego": [-117.1611, 32.7157],
   "Houston": [-95.3698, 29.7604],
-  "Starlight": [-87.6173, 41.8962],
+  "Chicago": [-87.6173, 41.8962],
   "Washington": [-77.0369, 38.9072],
   "TACC": [-97.743057, 30.267153],
   "LBNL": [null],
@@ -70,27 +71,29 @@ const coordinates = {
   "MGHPCC": [-72.6162, 42.2043],
   "NCSA": [-88.207458, 40.112461],
   "SDSC": [-117.2713, 32.8328],
+  "LBNL": [-122.2730, 37.8715]
 }
 
 const core_nodes = [
-  { markerOffset: 20, name: "Salt Lake City" },
-  { markerOffset: 20, name: "Kansas City" },
-  { markerOffset: 20, name: "New York" },
-  { markerOffset: 20, name: "Atlanta" },
-  { markerOffset: 20, name: "Seattle" },
-  { markerOffset: 20, name: "San Diego" },
+  { markerOffset: -20, name: "Salt Lake City" },
+  { markerOffset: -20, name: "Kansas City" },
+  { markerOffset: -20, name: "New York" },
+  { markerOffset: -20, name: "Atlanta" },
+  { markerOffset: -20, name: "Seattle" },
+  { markerOffset: 25, name: "San Diego" },
   { markerOffset: 20, name: "Houston" },
-  { markerOffset: 20, name: "Starlight" },
-  { markerOffset: 20, name: "Washington" },
+  { markerOffset: -20, name: "Chicago" },
+  { markerOffset: -20, name: "Washington" },
 ];
 
 const edge_nodes = [
-  { markerOffset: 15, name: "TACC" },
-  { markerOffset: 15, name: "NCSA" },
-  { markerOffset: -30, name: "CHAMELEON" },
-  { markerOffset: -30, name: "COSMOS" },
-  { markerOffset: -30, name:"MGHPCC" },
-  { markerOffset: -30, name:"SDSC" }
+  { markerOffset: -25, name: "TACC" },
+  { markerOffset: 5, name: "NCSA" },
+  { markerOffset: -25, name:"MGHPCC" },
+  { markerOffset: -25, name:"LBNL" },
+  // { markerOffset: -30, name:"SDSC" },
+  // { markerOffset: -30, name: "CHAMELEON" },
+  // { markerOffset: -30, name: "COSMOS" },
 ];
 
 
@@ -101,19 +104,112 @@ const lines_dedicated = [
   { from: "San Diego", to: "Houston" },
   { from: "Salt Lake City" , to: "Kansas City" },
   { from: "Houston", to: "Atlanta" },
-  { from: "Kansas City", to: "Starlight" },
+  { from: "Kansas City", to: "Chicago" },
   { from: "Kansas City", to: "Houston" },
-  { from: "Starlight", to: "Atlanta" },
-  { from: "Starlight", to: "Washington" },
-  { from: "Starlight", to: "New York" },
+  { from: "Chicago", to: "Atlanta" },
+  { from: "Chicago", to: "Washington" },
+  { from: "Chicago", to: "New York" },
   { from: "Washington", to: "New York"},
   { from: "Washington", to: "Atlanta"},
 ]
 
 const lines_super = [
-  { from: "Starlight", to:  "Washington" },
-  { from: "Houston", to: "Starlight" },
+  { from: "Chicago", to:  "Washington" },
+  { from: "Houston", to: "Chicago" },
   { from: "San Diego", to: "Houston" },
+]
+
+const annotations = [
+  {
+    city: "Chicago",
+    dx: -50,
+    dy: -35,
+    textAnchor: "end",
+    content: [
+      "STARLIGHT",
+      "CHAMELEON",
+      "MERIT",
+      "WISCNET",
+      "KYRON"
+    ]
+  },
+  {
+    city: "Salt Lake City",
+    dx: 30,
+    dy: 40,
+    textAnchor: "start",
+    content: [
+      "UEN",
+      "CLOUDLAB",
+      "POWDER",
+    ]
+  },
+  {
+    city: "San Diego",
+    dx: 40,
+    dy: -20,
+    textAnchor: "start",
+    content: [
+      "SDSC",
+      "PRP/NRP",
+      "CENIC",
+    ]
+  },
+  {
+    city: "Atlanta",
+    dx: 30,
+    dy: 20,
+    textAnchor: "start",
+    content: [
+      "AMLIGHT",
+      "SOX",
+      "FLR",
+    ]
+  },
+  {
+    city: "New York",
+    dx: 30,
+    dy: 30,
+    textAnchor: "start",
+    content: [
+      "COSMOS",
+      "NYSERNET",
+      "NEREN",
+      "NJEDGE"
+    ]
+  },
+  {
+    city: "Washington",
+    dx: 10,
+    dy: 30,
+    textAnchor: "start",
+    content: [
+      "3ROX",
+      "MAX",
+      "WIX",
+      "KINBER",
+      "NCREN"
+    ]
+  },
+  {
+    city: "Kansas City",
+    dx: -10,
+    dy: 30,
+    textAnchor: "end",
+    content: [
+      "FRGP",
+      "GPN",
+    ]
+  },
+  {
+    city: "Houston",
+    dx: 30,
+    dy: -10,
+    textAnchor: "start",
+    content: [
+      "LEARN",
+    ]
+  },
 ]
 
 export const MapModule = props => {
@@ -135,7 +231,6 @@ export const MapModule = props => {
                   {geographies.map(geo => (
                     <Geography
                       key={geo.rsmKey}
-                      // stroke="#FFF"
                       geography={geo}
                       fill="#cde4ef"
                     />
@@ -144,13 +239,48 @@ export const MapModule = props => {
               )}
             </Geographies>
 
+            {annotations.map(({city, dx, dy, textAnchor, content}) => {
+              let annotations_y = -10
+              return (
+              <Annotation
+                subject={coordinates[city]}
+                dx={dx}
+                dy={dy}
+                connectorProps={{
+                  stroke: "#FF5533",
+                  strokeWidth: 1,
+                  strokeLinecap: "round",
+                }}
+              >  
+                {
+                  content.map((c) => {
+                   annotations_y += 10;
+                   return(
+                    <text
+                      y={annotations_y}
+                      textAnchor={textAnchor}
+                      alignmentBaseline="middle"
+                      fill="#F53"
+                      style={{
+                        fontFamily: "system-ui",
+                        fill: "#f26522",
+                        fontSize: ".6rem",
+                        fontWeight: "600",
+                      }}>
+                     {c}
+                    </text>
+                  )})
+                }
+              </Annotation>
+            )})}
+
             {lines_dedicated.map(({ from, to }) => (
               <Line
                 key={`line-${from}-to-${to}`}
                 from={coordinates[from]}
                 to={coordinates[to]}
                 stroke="#c7c7c7"
-                strokeWidth={1}
+                strokeWidth={2}
                 strokeLinecap="round"
               />
             ))}
@@ -168,28 +298,21 @@ export const MapModule = props => {
 
             {core_nodes.map(({ name, markerOffset }) => (
               <Marker key={name} coordinates={coordinates[name]}>
-                <circle r={8} fill="#27aae1" strokeWidth={1.5} />
+                <circle r={8} fill="#27aae1" />
                 <text
                   textAnchor="middle"
                   y={markerOffset}
-                  style={{ fontFamily: "system-ui", fill: "#5D5A6D", fontSize: ".8rem" }}
+                  style={{ fontFamily: "system-ui", fill: "#5D5A6D", fontSize: ".8rem", fontWeight: "8600" }}
                 >
                   {name}
                 </text>
               </Marker>
             ))}
+
             {edge_nodes.map(({ name, markerOffset }) => (
               <Marker key={name} coordinates={coordinates[name]}>
-                <g
-                  fill="none"
-                  stroke="#f26522"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  transform="translate(-12, -24)"
-                >
-                  <circle cx="12" cy="10" r="3" />
-                  <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                <g transform="translate(-12, -24)">
+                  <circle cx="12" cy="10" r="5" fill="#f26522" />
                 </g>
                 <text
                   textAnchor="middle"
