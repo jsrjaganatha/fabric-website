1edce39942cc8351685ea993b015a4bcea7f2752
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2021-01-21 15:16:34 -0500

    Enhancement/14 add interactive map fabric topology (#37)
    * feature-new-leadership-page (#3)
    
    * add sample data and images for leadership page
    
    * add route/ menu for new leadership page
    
    * add leadership page and hook
    
    * add complete markdown data and images for new leadership page
    
    * added the filed of order to data & sort leaders in hook
    
    * fix menu dropdown item hover color issue & add key to sac/ leadership items
    
    * update Anit'a institution and corresponding logo in partners list
    
    add u of i logo
    
    update institution
    
    remove iit logo
    
    pull in new u of i logo for partners module
    
    * Link RedHat Research Days in Event (#7)
    
    * update Anit'a institution and corresponding logo in partners list (#5)
    
    add u of i logo
    
    update institution
    
    remove iit logo
    
    pull in new u of i logo for partners module
    
    * Link the RedHat Research days event
    
    * #14: added basic USA state map
    
    * #14: added city markers by coordinates
    
    * #14: added edge node markers
    
    * #14: applied tabs to 3 map phases
    
    * #14: added annotations for edge nodes
    
    * #14: created subcomponents for map
    
    * #14: added global map for FAB phase
    
    * #14: used the same map background for phase 1 & 2
    
    * #14: added zoomable group
    
    * #14: added USA nodes to FAB phase
    
    * #14: added map legend to phase 1 & 2
    
    * #14: added example tooltip
    
    * #14: added annotations for 'Internet2 & AWS'
    
    * #14: modified map size and added legend to FAB phase
    
    * #14: updated fab phase map and legend
    
    * #14: updated phase 2 map and legend
    
    * #14: updated nodes and links for phase 1 && updated map legends
    
    * #14: updated nodes in phase 1
    
    * #14: extracted common map data
    
    * #14: updated map data
    
    * add CC* news article (#34)
    
    * add article
    
    * edit article metadata
    
    update date
    
    fix newline
    
    * add tag
    
    * markdown underscore/link fix
    
    * markdown underscore/link fix, number 2
    
    * #14: added high-bandwidth l2 links with dotted lines in phase 1
    
    * add FAB logos (#35)
    
    * prep fab logo section
    
    * add FAB logos to branding/logos view
    
    * #14: ----- map version 2 -----
    
    * #14: ----- map version 3 -----
    
    * #14: solved merge conflicts
    
    * #14: change requested: removed 'fontFamily: system-ui' for all node style so the map falls back on the site's default primary font
    
    * #14: deleted node_modules and re-installed dependencies
    
    * Revert "#14: change requested: removed 'fontFamily: system-ui' for all node style so the map falls back on the site's default primary font"
    
    This reverts commit febe6264d771de6f562ae9dcf259879d44511912.
    
    Co-authored-by: Matt Watson <mattwat@gmail.com>

--- src/components/modules/map.js
+++ src/components/modules/map.js
@@ -1,11 +1,180 @@
-import React from "react"
-import { Module } from "../layout"
-import fabricMapSvg from "../../images/fabric-map.svg"
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Button } from '../button'
+import { Module } from '../layout'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Line,
+} from "react-simple-maps";
 
-export const MapModule = (props) => {
-  return (
-    <Module title="Anticipated FABRIC Topology">
-      <img src={fabricMapSvg} alt="" style={{ width: "100%" }} />
-    </Module>
-  )
+import { MapPhase3 } from './map-phase-3'
+import fabricMapLegend1 from "../../images/fabric-map/map-legend-phase1.png"
+import fabricMapLegend2 from "../../images/fabric-map/map-legend-phase2.png"
+
+import { topomap } from "../../data/topomap.js"
+
+const geoUrl = {
+  "phase1": "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json",
+  "phase2": "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json",
+};
+
+const Tabs = styled.article`
+    display: flex;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`
+const Tab = styled(Button)`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    : transform 250ms ease-out;
+    cursor: pointer;
+    padding: ${ props => props.compact ? '0.25rem' : '0.5rem 1rem' };
+    margin: 0 0.25rem;
+    border-width: 1px;
+    border-style: solid;
+    border-color: var(--color-primary);
+    border-radius: 0;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    box-shadow: none;
+    background-color: ${ props => props.active ? 'var(--color-primary)' : 'var(--color-light)' };
+    color: ${ props => props.active ? 'var(--color-light)' : 'var(--color-primary)' };
+    &:hover, &:focus {
+        background-color: ${ props => props.active ? 'var(--color-primary)' : 'var(--color-primary-light)' } !important;
+        color: ${ props => props.active ? 'var(--color-light)' : 'var(--color-primary-dark)' } !important;
+        box-shadow: none;
+    }
+`
+
+export const MapModule = props => {
+    const [tabIndex, setTabIndex] = useState(1)
+    const dataset = ["phase1", "phase2"]
+    const handleToggleTab = newIndex => event => setTabIndex(newIndex)
+    return (
+      <Module title="Anticipated FABRIC Topology" className="fabric-map-container">
+          <Tabs>
+            <Tab key="map-phase-1" onClick={ handleToggleTab(0) } active={ tabIndex === 0 }>Phase 1</Tab>
+            <Tab key="map-phase-2" onClick={ handleToggleTab(1) } active={ tabIndex === 1 }>Phase 2</Tab>
+            <Tab key="map-phase-3" onClick={ handleToggleTab(2) } active={ tabIndex === 2 }>FABRIC Across Borders</Tab>
+          </Tabs>
+          <div className="fabric-map">
+            { tabIndex === 2 && <MapPhase3 />}
+            { tabIndex !== 2 && 
+            <ComposableMap projection="geoAlbersUsa" width={800} height={500} >
+              <Geographies geography={geoUrl[dataset[tabIndex]]}>
+                {({ geographies }) => (
+                  <>
+                    {geographies.map(geo => (
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        fill="#cde4ef"
+                      />
+                    ))}
+                  </>
+                )}
+              </Geographies>
+
+              {
+                tabIndex === 0 && topomap.dotted_lines.map(({ from, to }) => (
+                  <Line
+                    key={`dotted-line-${from}-to-${to}`}
+                    from={topomap.coordinates[from]}
+                    to={topomap.coordinates[to]}
+                    stroke="#27aae1"
+                    strokeWidth={2}
+                    strokeDasharray="3"
+                  />
+                ))
+              }
+
+              {topomap.lines[dataset[tabIndex]].map(({ from, to }) => (
+                <Line
+                  key={`line-${from}-to-${to}`}
+                  from={topomap.coordinates[from]}
+                  to={topomap.coordinates[to]}
+                  stroke="#27aae1"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+              ))}
+
+            {
+              topomap.lines_super[dataset[tabIndex]] &&
+              topomap.lines_super[dataset[tabIndex]].map(({ from, to }) => (
+                <Line
+                  key={`super-line-${from}-to-${to}`}
+                  from={topomap.coordinates[from]}
+                  to={topomap.coordinates[to]}
+                  stroke="#ffde17"
+                  strokeWidth={8}
+                  strokeLinecap="round"
+                />
+              ))
+              }
+
+              {topomap.core_nodes[dataset[tabIndex]].map(({ name, markerOffset }) => (
+                <Marker key={name} coordinates={topomap.coordinates[name]} data-tip="Core Connectors: 10">
+                  <circle r={6} fill="#078ac1" />
+                  <text
+                    textAnchor="middle"
+                    y={markerOffset}
+                    style={{ fontFamily: "system-ui", fill: "#5D5A6D", fontSize: ".7rem", fontWeight: "800" }}
+                  >
+                    {name}
+                  </text>
+                </Marker>
+              ))}
+
+              {topomap.edge_nodes[dataset[tabIndex]].map(({ name, markerOffset }) => (
+                <Marker key={name} coordinates={topomap.coordinates[name]} data-tip="Core Connectors: 10">
+                  <circle r={3} fill="#27aae1" />
+                  <text
+                    textAnchor="middle"
+                    y={markerOffset}
+                    style={{ fontFamily: "system-ui", fill: "#5D5A6D", fontSize: ".5rem", fontWeight: "600" }}
+                  >
+                    {name}
+                  </text>
+                </Marker>
+              ))}
+
+              {
+                topomap.facilities[dataset[tabIndex]] &&
+                topomap.facilities[dataset[tabIndex]].map(({ name, markerOffset }) => (
+                  <Marker key={name} coordinates={topomap.coordinates[name]}>
+                    <g
+                      fill="none"
+                      stroke="#FF5533"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      transform="translate(-12, -24)"
+                    >
+                      <circle cx="12" cy="10" r="3" fill="#f26522"/>
+                      <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                    </g>
+                    <text
+                      textAnchor="middle"
+                      y={markerOffset}
+                      style={{ fontFamily: "system-ui", fill: "#f26522", fontSize: ".5rem", fontWeight: "600" }}
+                    >
+                      {name}
+                    </text>
+                  </Marker>
+                ))
+              }
+        </ComposableMap>
+        }
+        </div>
+        { tabIndex === 0 && <img src={fabricMapLegend1} alt="" style={{ width: "80%" }} /> }
+        { tabIndex === 1 && <img src={fabricMapLegend2} alt="" style={{ width: "80%" }} /> }
+      </Module>
+    )
 }
